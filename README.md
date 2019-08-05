f6f5399d13d0d512f167bcc00cdfe0a64ad8b22f
Andre Levi <andrelevi09@gmail.com>
2019-08-05 13:53:09 +0700

    Add option for converting gamma to linear and vice versa

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Example Usage
 `./add-image-to-alpha-channel.py source.png smoothness.png smoothness`
+
+Linear to Gamma
+`./add-image-to-alpha-channel.py source.png smoothness.png smoothness 0.454545`
+
+Gamma to Linear
+`./add-image-to-alpha-channel.py source.png smoothness.png smoothness 2.2`
--- add-image-to-alpha-channel.py
+++ add-image-to-alpha-channel.py
@@ -5,13 +5,19 @@ from tkinter import Tk, filedialog
 import os
 import sys
 
-if len(sys.argv) != 4:
-    print("Bad arguments. Args: RGB image, alpha channel image and suffix.")
+if len(sys.argv) < 4:
+    print("Bad arguments. Args: RGB image, alpha channel image, suffix, exponent power.")
     exit()
 
 rgb_file_name = sys.argv[1]
 alpha_image_name = sys.argv[2]
 alpha_type = sys.argv[3]
+
+if len(sys.argv) != 5: 
+    exponent_power = sys.argv[4]
+else: 
+    exponent_power = 1
+
 print("Image to modify: " + rgb_file_name)
 print("Image to add as alpha channel: " + alpha_image_name)
 
@@ -30,6 +36,12 @@ alpha_image = alpha_image.convert("RGB")
 r,g,b = alpha_image.split()
 
 alpha = r.convert("L")
+
+if exponent_power != 1:
+    for x in range(alpha.width):
+        for y in range(alpha.height):
+            altered_pixel = pow(alpha.getpixel((x,y)), exponent_power);
+            alpha.putpixel((x, y), altered_pixel)
 
 rgb_image.putalpha(alpha)
 
